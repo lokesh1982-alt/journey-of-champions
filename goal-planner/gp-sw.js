ce792bbd689128e77604d9efda2f7915b82fd4c5
lokesh1982-alt <goset.lokesh@gmail.com>
2026-06-19 21:30:30 +0100

    Add files via upload

---
+++ goal-planner/gp-sw.js
@@ -0,0 +1,4 @@
+const CACHE='goal-planner-v1';
+const ASSETS=['./goal-planner.html','./gp-manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
